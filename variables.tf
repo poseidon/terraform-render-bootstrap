3720aff28a465987e079dcd74fe3b6d5046d7010
Dalton Hubble <dghubble@gmail.com>
2017-05-19 16:26:52 -0700

    Bump hyperkube to v1.6.4_coreos.0 for bootkube v0.4.4

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable "container_images" {
   type        = "map"
 
   default = {
-    hyperkube = "quay.io/coreos/hyperkube:v1.6.2_coreos.0"
+    hyperkube = "quay.io/coreos/hyperkube:v1.6.4_coreos.0"
     etcd = "quay.io/coreos/etcd:v3.1.6"
   }
 }
