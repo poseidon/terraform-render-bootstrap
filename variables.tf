5072569bb7dff1c2f6bc6fb7b06ce0a41809971e
Dalton Hubble <dghubble@gmail.com>
2017-12-21 11:16:29 -0800

    Update calico/cni sidecar from v1.11.1 to v1.11.2

--- variables.tf
+++ variables.tf
@@ -64,7 +64,7 @@ variable "container_images" {
 
   default = {
     calico           = "quay.io/calico/node:v2.6.4"
-    calico_cni       = "quay.io/calico/cni:v1.11.1"
+    calico_cni       = "quay.io/calico/cni:v1.11.2"
     flannel          = "quay.io/coreos/flannel:v0.9.1-amd64"
     flannel_cni      = "quay.io/coreos/flannel-cni:v0.3.0"
     hyperkube        = "gcr.io/google_containers/hyperkube:v1.8.6"
