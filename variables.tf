0daa1276c633fea28e41b2c2c18831e2584deb24
Dalton Hubble <dghubble@gmail.com>
2019-11-13 13:02:01 -0800

    Update Kubernetes from v1.16.2 to v1.16.3
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.16.md#v1163

--- variables.tf
+++ variables.tf
@@ -74,7 +74,7 @@ variable "container_images" {
     flannel     = "quay.io/coreos/flannel:v0.11.0-amd64"
     flannel_cni = "quay.io/coreos/flannel-cni:v0.3.0"
     kube_router = "cloudnativelabs/kube-router:v0.3.2"
-    hyperkube   = "k8s.gcr.io/hyperkube:v1.16.2"
+    hyperkube   = "k8s.gcr.io/hyperkube:v1.16.3"
     coredns     = "k8s.gcr.io/coredns:1.6.2"
   }
 }
