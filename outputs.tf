50a221e042f0e4d51f1c86739c4d27b969ee5c1f
Dalton Hubble <dghubble@gmail.com>
2019-12-05 00:57:07 -0800

    Annotate sensitive output variables to suppress display
    * Annotate terraform output variables containing generated TLS
    credentials and kubeconfigs as sensitive to suppress / mask
    them in terraform CLI display.
    * Allow for easier use in automation systems and logged environments

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,13 @@ output "cluster_dns_service_ip" {
 // Generated kubeconfig for Kubelets (i.e. lower privilege than admin)
 output "kubeconfig-kubelet" {
   value = data.template_file.kubeconfig-kubelet.rendered
+  sensitive = true
 }
 
 // Generated kubeconfig for admins (i.e. human super-user)
 output "kubeconfig-admin" {
   value = data.template_file.kubeconfig-admin.rendered
+  sensitive = true
 }
 
 # assets to distribute to controllers
@@ -28,34 +30,42 @@ output "assets_dist" {
     local.calico_manifests,
     local.kube_router_manifests,
   )
+  sensitive = true
 }
 
 # etcd TLS assets
 
 output "etcd_ca_cert" {
   value = tls_self_signed_cert.etcd-ca.cert_pem
+  sensitive = true
 }
 
 output "etcd_client_cert" {
   value = tls_locally_signed_cert.client.cert_pem
+  sensitive = true
 }
 
 output "etcd_client_key" {
   value = tls_private_key.client.private_key_pem
+  sensitive = true
 }
 
 output "etcd_server_cert" {
   value = tls_locally_signed_cert.server.cert_pem
+  sensitive = true
 }
 
 output "etcd_server_key" {
   value = tls_private_key.server.private_key_pem
+  sensitive = true
 }
 
 output "etcd_peer_cert" {
   value = tls_locally_signed_cert.peer.cert_pem
+  sensitive = true
 }
 
 output "etcd_peer_key" {
   value = tls_private_key.peer.private_key_pem
+  sensitive = true
 }
