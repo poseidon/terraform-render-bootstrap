1ea8fe7a85e87f7e851b4cd4ab8c4e9902b4a40c
Dalton Hubble <dghubble@gmail.com>
2020-02-06 00:03:00 -0800

    Update Calico from v3.11.2 to v3.12.0
    * https://docs.projectcalico.org/release-notes/#v3120

--- variables.tf
+++ variables.tf
@@ -70,8 +70,8 @@ variable "container_images" {
   description = "Container images to use"
 
   default = {
-    calico      = "quay.io/calico/node:v3.11.2"
-    calico_cni  = "quay.io/calico/cni:v3.11.2"
+    calico      = "quay.io/calico/node:v3.12.0"
+    calico_cni  = "quay.io/calico/cni:v3.12.0"
     flannel     = "quay.io/coreos/flannel:v0.11.0-amd64"
     flannel_cni = "quay.io/coreos/flannel-cni:v0.3.0"
     kube_router = "cloudnativelabs/kube-router:v0.3.2"
