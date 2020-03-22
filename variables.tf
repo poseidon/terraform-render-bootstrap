cb170f802d09dcbe88b050257bc676e25d3c4282
Dalton Hubble <dghubble@gmail.com>
2020-03-22 13:39:13 -0700

    Update Kubernetes from v1.17.4 to v1.18.0
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.18.md

--- variables.tf
+++ variables.tf
@@ -75,10 +75,10 @@ variable "container_images" {
     coredns                 = "k8s.gcr.io/coredns:1.6.7"
     flannel                 = "quay.io/coreos/flannel:v0.11.0-amd64"
     flannel_cni             = "quay.io/coreos/flannel-cni:v0.3.0"
-    kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.17.4"
-    kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.17.4"
-    kube_scheduler          = "k8s.gcr.io/kube-scheduler:v1.17.4"
-    kube_proxy              = "k8s.gcr.io/kube-proxy:v1.17.4"
+    kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.18.0"
+    kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.18.0"
+    kube_scheduler          = "k8s.gcr.io/kube-scheduler:v1.18.0"
+    kube_proxy              = "k8s.gcr.io/kube-proxy:v1.18.0"
     # experimental
     kube_router = "cloudnativelabs/kube-router:v0.3.2"
   }
