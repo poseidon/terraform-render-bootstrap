fc1a7bac89c6c95082334e0f79e478ee321596c0
Dalton Hubble <dghubble@gmail.com>
2020-06-11 21:18:07 -0700

    Remove unused Kubelet certificate and key pair
    * Kubelet certificate and key pair in state (not distributed)
    are not needed after with Kubelet TLS bootstrap
    * https://github.com/poseidon/terraform-render-bootstrap/pull/185
    
    Fix https://github.com/poseidon/typhoon/issues/757

--- tls-k8s.tf
+++ tls-k8s.tf
@@ -195,34 +195,3 @@ resource "tls_cert_request" "kubelet" {
   }
 }
 
-resource "tls_locally_signed_cert" "kubelet" {
-  cert_request_pem = tls_cert_request.kubelet.cert_request_pem
-
-  ca_key_algorithm   = tls_self_signed_cert.kube-ca.key_algorithm
-  ca_private_key_pem = tls_private_key.kube-ca.private_key_pem
-  ca_cert_pem        = tls_self_signed_cert.kube-ca.cert_pem
-
-  validity_period_hours = 8760
-
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-    "client_auth",
-  ]
-}
-
-resource "local_file" "kubelet-key" {
-  count = var.asset_dir == "" ? 0 : 1
-
-  content  = tls_private_key.kubelet.private_key_pem
-  filename = "${var.asset_dir}/tls/kubelet.key"
-}
-
-resource "local_file" "kubelet-crt" {
-  count = var.asset_dir == "" ? 0 : 1
-
-  content  = tls_locally_signed_cert.kubelet.cert_pem
-  filename = "${var.asset_dir}/tls/kubelet.crt"
-}
-
