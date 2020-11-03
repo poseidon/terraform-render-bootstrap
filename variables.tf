c6e3a2bcdc06aff31b1f83c91b2f0af4f778b86f
Dalton Hubble <dghubble@gmail.com>
2020-11-03 00:05:32 -0800

    Update Cilium from v1.8.5 to v1.9.0-rc3
    * https://github.com/cilium/cilium/releases/tag/v1.9.0-rc3
    * https://github.com/cilium/cilium/releases/tag/v1.9.0-rc2
    * https://github.com/cilium/cilium/releases/tag/v1.9.0-rc1

--- variables.tf
+++ variables.tf
@@ -66,8 +66,8 @@ variable "container_images" {
   default = {
     calico                  = "quay.io/calico/node:v3.16.4"
     calico_cni              = "quay.io/calico/cni:v3.16.4"
-    cilium_agent            = "docker.io/cilium/cilium:v1.8.5"
-    cilium_operator         = "docker.io/cilium/operator-generic:v1.8.5"
+    cilium_agent            = "docker.io/cilium/cilium:v1.9.0-rc3"
+    cilium_operator         = "docker.io/cilium/operator-generic:v1.9.0-rc3"
     coredns                 = "k8s.gcr.io/coredns:1.7.0"
     flannel                 = "quay.io/coreos/flannel:v0.13.0"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.1"
