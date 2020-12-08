94a0e6d7f5b6a03bf7a08f2538791f66efffb1d9
Ben Drucker <bvdrucker@gmail.com>
2020-12-08 10:21:39 -0800

    versions: allow v3 of tls and random providers

--- versions.tf
+++ versions.tf
@@ -3,8 +3,8 @@
 terraform {
   required_version = ">= 0.13.0, < 0.15.0"
   required_providers {
-    random   = "~> 2.2"
+    random   = ">= 2, < 4"
     template = "~> 2.1"
-    tls      = "~> 2.0"
+    tls      = ">= 2, < 4"
   }
 }
