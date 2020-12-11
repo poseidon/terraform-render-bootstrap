4edd79dd0295e6ffa4c8ed04fd5914d5cb3f1b7c
Dalton Hubble <dghubble@gmail.com>
2020-12-10 22:47:40 -0800

    Update Calico from v3.17.0 to v3.17.1
    * https://github.com/projectcalico/calico/releases/tag/v3.17.1

--- variables.tf
+++ variables.tf
@@ -64,8 +64,8 @@ variable "container_images" {
   description = "Container images to use"
 
   default = {
-    calico                  = "quay.io/calico/node:v3.17.0"
-    calico_cni              = "quay.io/calico/cni:v3.17.0"
+    calico                  = "quay.io/calico/node:v3.17.1"
+    calico_cni              = "quay.io/calico/cni:v3.17.1"
     cilium_agent            = "quay.io/cilium/cilium:v1.9.1"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.9.1"
     coredns                 = "k8s.gcr.io/coredns:1.7.0"
