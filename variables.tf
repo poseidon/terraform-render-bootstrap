0b102c40895361bfe0cf7511a55bd75e8c648460
Dalton Hubble <dghubble@gmail.com>
2021-10-05 19:33:09 -0700

    Update Calico from v3.20.1 to v3.20.2
    * https://github.com/projectcalico/calico/releases/tag/v3.20.2
    * Add support for iptables legacy vs nft detection

--- variables.tf
+++ variables.tf
@@ -58,8 +58,8 @@ variable "container_images" {
   description = "Container images to use"
 
   default = {
-    calico                  = "quay.io/calico/node:v3.20.1"
-    calico_cni              = "quay.io/calico/cni:v3.20.1"
+    calico                  = "quay.io/calico/node:v3.20.2"
+    calico_cni              = "quay.io/calico/cni:v3.20.2"
     cilium_agent            = "quay.io/cilium/cilium:v1.10.4"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.10.4"
     coredns                 = "k8s.gcr.io/coredns/coredns:v1.8.4"
