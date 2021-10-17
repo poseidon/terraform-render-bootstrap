6b5d0887956cea3ea4ac0b93d9a3c31944e18dc0
Dalton Hubble <dghubble@gmail.com>
2021-10-17 11:22:59 -0700

    Update Cilium from v1.10.4 to v1.10.5
    * https://github.com/cilium/cilium/releases/tag/v1.10.5

--- variables.tf
+++ variables.tf
@@ -60,8 +60,8 @@ variable "container_images" {
   default = {
     calico                  = "quay.io/calico/node:v3.20.2"
     calico_cni              = "quay.io/calico/cni:v3.20.2"
-    cilium_agent            = "quay.io/cilium/cilium:v1.10.4"
-    cilium_operator         = "quay.io/cilium/operator-generic:v1.10.4"
+    cilium_agent            = "quay.io/cilium/cilium:v1.10.5"
+    cilium_operator         = "quay.io/cilium/operator-generic:v1.10.5"
     coredns                 = "k8s.gcr.io/coredns/coredns:v1.8.4"
     flannel                 = "quay.io/coreos/flannel:v0.13.0"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.2"
