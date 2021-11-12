e6193bbdcfe90fd3c18b3a9dc4bdedb0a49ece98
Dalton Hubble <dghubble@gmail.com>
2021-11-12 10:59:20 -0800

    Update CoreDNS from v1.8.4 to v1.8.6
    * https://github.com/kubernetes/kubernetes/pull/106091

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "container_images" {
     calico_cni              = "quay.io/calico/cni:v3.21.0"
     cilium_agent            = "quay.io/cilium/cilium:v1.10.5"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.10.5"
-    coredns                 = "k8s.gcr.io/coredns/coredns:v1.8.4"
+    coredns                 = "k8s.gcr.io/coredns/coredns:v1.8.6"
     flannel                 = "quay.io/coreos/flannel:v0.15.0"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.2"
     kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.22.3"
