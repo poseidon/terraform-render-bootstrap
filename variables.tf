3db4055ccfbd30d1880eaa2c79cf53f9a9d7d3f2
Dalton Hubble <dghubble@gmail.com>
2022-10-20 09:25:53 -0700

    Update Calico from v3.24.1 to v3.24.2
    * https://github.com/projectcalico/calico/releases/tag/v3.24.2

--- variables.tf
+++ variables.tf
@@ -60,8 +60,8 @@ variable "container_images" {
   description = "Container images to use"
 
   default = {
-    calico                  = "quay.io/calico/node:v3.24.1"
-    calico_cni              = "quay.io/calico/cni:v3.24.1"
+    calico                  = "quay.io/calico/node:v3.24.2"
+    calico_cni              = "quay.io/calico/cni:v3.24.2"
     cilium_agent            = "quay.io/cilium/cilium:v1.12.3"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.12.3"
     coredns                 = "registry.k8s.io/coredns/coredns:v1.9.3"
