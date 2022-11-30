616069203eb67fc47f77866869a0b2340435d197
Dalton Hubble <dghubble@gmail.com>
2022-11-30 08:45:09 -0800

    Update Kubernetes from v1.25.4 to v1.26.0-rc.0
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.26.md#v1260-rc0

--- variables.tf
+++ variables.tf
@@ -67,10 +67,10 @@ variable "container_images" {
     coredns                 = "registry.k8s.io/coredns/coredns:v1.9.3"
     flannel                 = "docker.io/flannelcni/flannel:v0.20.1"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.2"
-    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.25.4"
-    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.25.4"
-    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.25.4"
-    kube_proxy              = "registry.k8s.io/kube-proxy:v1.25.4"
+    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.26.0-rc.0"
+    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.26.0-rc.0"
+    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.26.0-rc.0"
+    kube_proxy              = "registry.k8s.io/kube-proxy:v1.26.0-rc.0"
   }
 }
 
