7f9853fca328fa7e80f408fa0a254d7c170d3fa0
Dalton Hubble <dghubble@gmail.com>
2023-02-09 09:55:02 -0800

    Update flannel from v0.20.2 to v0.21.1
    * https://github.com/flannel-io/flannel/releases/tag/v0.21.1

--- variables.tf
+++ variables.tf
@@ -65,7 +65,7 @@ variable "container_images" {
     cilium_agent            = "quay.io/cilium/cilium:v1.12.6"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.12.6"
     coredns                 = "registry.k8s.io/coredns/coredns:v1.9.4"
-    flannel                 = "docker.io/flannelcni/flannel:v0.20.2"
+    flannel                 = "docker.io/flannelcni/flannel:v0.21.1"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.2"
     kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.26.1"
     kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.26.1"
