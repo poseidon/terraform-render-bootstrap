452bcf379d94f5d479c1dee281fd479872271415
Dalton Hubble <dghubble@gmail.com>
2024-05-15 21:56:50 -0700

    Update Kubernetes from v1.30.0 to v1.30.1
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.30.md#v1301

--- variables.tf
+++ variables.tf
@@ -65,10 +65,10 @@ variable "container_images" {
     coredns                 = "registry.k8s.io/coredns/coredns:v1.9.4"
     flannel                 = "docker.io/flannel/flannel:v0.25.1"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.4"
-    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.30.0"
-    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.30.0"
-    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.30.0"
-    kube_proxy              = "registry.k8s.io/kube-proxy:v1.30.0"
+    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.30.1"
+    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.30.1"
+    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.30.1"
+    kube_proxy              = "registry.k8s.io/kube-proxy:v1.30.1"
   }
 }
 
