1cfc6544945e7c178d6a69be2439a01e060d3528
Dalton Hubble <dghubble@gmail.com>
2024-09-20 14:29:33 -0700

    Update Kubernetes from v1.30.0 to v1.30.1
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.31.md#v1311

--- variables.tf
+++ variables.tf
@@ -67,12 +67,12 @@ variable "container_images" {
     cilium_agent            = "quay.io/cilium/cilium:v1.16.1"
     cilium_operator         = "quay.io/cilium/operator-generic:v1.16.1"
     coredns                 = "registry.k8s.io/coredns/coredns:v1.11.3"
-    flannel                 = "docker.io/flannel/flannel:v0.25.5"
+    flannel                 = "docker.io/flannel/flannel:v0.25.6"
     flannel_cni             = "quay.io/poseidon/flannel-cni:v0.4.2"
-    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.31.0"
-    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.31.0"
-    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.31.0"
-    kube_proxy              = "registry.k8s.io/kube-proxy:v1.31.0"
+    kube_apiserver          = "registry.k8s.io/kube-apiserver:v1.31.1"
+    kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.31.1"
+    kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.31.1"
+    kube_proxy              = "registry.k8s.io/kube-proxy:v1.31.1"
   }
 }
 
